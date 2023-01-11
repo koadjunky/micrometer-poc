2ed675bf1b0127741d0c944a449b530ab82658e4
Maciej Malycha <koadjunky@gmail.com>
2023-01-11 19:21:50 +0100

    Added gradle, bootstrapped springboot

---
+++ src/main/java/eu/malycha/micrometerpoc/MicrometerPocApplication.java
@@ -0,0 +1,13 @@
+package eu.malycha.micrometerpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MicrometerPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrometerPocApplication.class, args);
+    }
+}
---
+++ src/test/java/eu/malycha/micrometerpoc/MicrometerPocApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.malycha.micrometerpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class MicrometerPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
