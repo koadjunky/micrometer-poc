e10455c0122adbdf041fc9e194ecd8834715d0c0
Maciej Malycha <koadjunky@gmail.com>
2023-02-06 23:44:41 +0100

    Added gauge

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # micrometer-poc
 
-Grafana dashboard 'JVM Micrometer' can be found at http://localhost:3000/
+## Description
+
+Project demonstrates how to integrate SpringBoot Actuator, Micrometer, Prometheus and Grafana
+and implement basic meters:
+ - counter - value that increments
+ - timer - duration of some action
+ - gauge - current value of meter
+
+## How to start
+
+Run docker compose:
+
+```docker compose up```
+
+Run application:
+
+```./gradlew clean bootRun```
+
+Observe Grafana dashboard 'JVM Micrometer' at http://localhost:3000/
--- src/main/java/eu/malycha/micrometerpoc/MeteredService.java
+++ src/main/java/eu/malycha/micrometerpoc/MeteredService.java
@@ -1,6 +1,7 @@
 package eu.malycha.micrometerpoc;
 
 import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Timer;
 import org.slf4j.Logger;
@@ -8,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.time.Duration;
 import java.util.Random;
 
@@ -18,17 +20,25 @@ public class MeteredService {
 
     private final Counter serviceCounter;
     private final Timer serviceTimer;
+    private final Gauge serviceGauge;
     private final Random random = new Random();
+
+    private double value;
 
     public MeteredService(MeterRegistry meterRegistry) {
         this.serviceCounter = meterRegistry.counter("service.counter", "type", "light");
         this.serviceTimer = meterRegistry.timer("service.timer", "type", "light");
+        // Non-fluent gauge API is kind of hard to understand. Fluent works better.
+        this.serviceGauge = Gauge.builder("service.gauge", this, s -> s.value)
+            .tag("type", "light")
+            .register(meterRegistry);
     }
 
     @Scheduled(fixedRate = 100)
     public void perform() {
         serviceCounter.increment();
         serviceTimer.record(Duration.ofMillis(random.nextInt(1000)));
+        value = random.nextDouble();
         LOGGER.info("Performing operation");
     }
 }
